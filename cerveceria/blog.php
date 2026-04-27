94b5b86d2b80db4c07a9b538e7d62c5309e8e1b1
Izan <izanlaraserranoserrano@gmail.com>
2026-04-27 13:37:53 +0200

    Añadida funcionalidad a blog.php y detalle.php

--- cerveceria/blog.php
+++ cerveceria/blog.php
@@ -1,3 +1,14 @@
+<?php
+    ini_set('display_startup_errors', 1);
+    ini_set('display_errors', 1);
+    error_reporting(-1);
+    
+    require_once "conexion.php";
+    
+    $sql_blogs = "SELECT * FROM blogs ORDER BY id DESC LIMIT 4";
+    $obtener_blogs = $conexion->query($sql_blogs);
+?>
+
 <!DOCTYPE html>
 <html>
     <head>
@@ -44,62 +55,27 @@ https://templatemo.com/tm-507-victory
     <section class="blog-page">
         <div class="container">
             <div class="row">
-                <div class="col-md-8 col-md-offset-2">
-                    <div class="blog-item">
-                        <img src="img/blog_item_01.jpg" alt="">
-                        <div class="date">26 Oct 2020</div>
-                        <div class="down-content">
-                            <h4>Nullam ornare purus massa</h4>
-                            <span>Branding / Admin</span>
-                            <p>Vivamus venenatis mi enim, ut gravida sem viverra sit amet. Nam ullamcorper dui nec risus malesuada fringilla. Aliquam erat volutpat. Aliquam a varius odio. Quisque iaculis massa vel nunc porta vehicula. Nulla consectetur iaculis elit. Vivamus euismod lorem rutrum iaculis commodo. Cras congue nisi non varius tincidunt.</p>
-                            <div class="text-button">
-                                <a href="#">Continue Reading</a>
+                <?php
+                if($obtener_blogs->num_rows > 0) {
+                    while($fila=$obtener_blogs->fetch_assoc()) { ?>
+                        <div class="col-md-8 col-md-offset-2">
+                            <div class="blog-item">
+                                <img src="img/blog_item_01.jpg" alt="">
+                                <div class="date"><?php echo $fila['fecha'] ?></div>
+                                <div class="down-content">
+                                    <h4><?php echo $fila['titulo'] ?></h4>
+                                    <p><?php echo $fila['texto'] ?></p>
+                                    <div class="text-button">
+                                        <a href="detalle.php?id_blog=<?php echo $fila['id'] ?>">+ Leer más</a>
+                                    </div>
+                                </div>
                             </div>
                         </div>
-                    </div>
-                </div>
-                <div class="col-md-8 col-md-offset-2">
-                    <div class="blog-item">
-                        <img src="img/blog_item_02.jpg" alt="">
-                        <div class="date">21 Oct 2020</div>
-                        <div class="down-content">
-                            <h4>Class aptent taciti sociosqu</h4>
-                            <span>Branding / Admin</span>
-                            <p>Praesent id pellentesque est. Etiam vestibulum eros quis vulputate convallis. Praesent quam diam, placerat a ipsum sed, facilisis dignissim lorem. Vivamus a elit vitae mauris fringilla scelerisque sit amet eget mauris. Suspendisse vulputate congue eleifend.</p>
-                            <div class="text-button">
-                                <a href="#">Continue Reading</a>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-                <div class="col-md-8 col-md-offset-2">
-                    <div class="blog-item">
-                        <img src="img/blog_item_03.jpg" alt="">
-                        <div class="date">11 Oct 2020</div>
-                        <div class="down-content">
-                            <h4>Cras congue nisi non varius tincidunt</h4>
-                            <span>Desserts / Chef</span>
-                            <p>Mauris mollis urna sit amet eros pretium, a tincidunt tellus eleifend. Sed dictum sit amet sapien ut scelerisque. Duis vulputate elit vehicula nisl maximus eleifend. Suspendisse potenti. Aenean ut dui fermentum, pharetra quam vitae, placerat dolor. Curabitur at dolor sed felis lacinia ultricies sit amet vel sem.</p>
-                            <div class="text-button">
-                                <a href="#">Continue Reading</a>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-                <div class="col-md-8 col-md-offset-2">
-                    <div class="blog-item">
-                        <img src="img/blog_item_04.jpg" alt="">
-                        <div class="date">03 Oct 2020</div>
-                        <div class="down-content">
-                            <h4>Vivamus euismod lorem</h4>
-                            <span>Food / Chef</span>
-                            <p>Pellentesque nec orci in erat venenatis viverra. Vivamus in lorem et leo feugiat ullamcorper. Donec volutpat fermentum erat non aliquet. Cras fermentum, risus a blandit sodales, erat turpis eleifend lacus, venenatis mollis leo lorem vel eros. Quisque et sem tempus, feugiat urna iaculis, tempor metus.</p>
-                            <div class="text-button">
-                                <a href="#">Continue Reading</a>
-                            </div>
-                        </div>
-                    </div>
-                </div>
+                <?php 
+                    }
+                } ?>
+                
+    
                 <div class="col-md-8 col-md-offset-2">
                     <ul class="page-number">
                         <li><a href="#">1</a></li>
---
+++ cerveceria/detalle.php
@@ -0,0 +1,158 @@
+<?php
+    ini_set('display_startup_errors', 1);
+    ini_set('display_errors', 1);
+    error_reporting(-1);
+
+    require_once "conexion.php";
+
+    if (isset($_GET['id_blog'])) {
+        $id_blog = $_GET['id_blog'];
+        $sql_blog = "SELECT * FROM blogs WHERE id=$id_blog";
+    }
+    // Ejecución
+    $obtener_detalles = $conexion->query($sql_blog);
+?>
+
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+<!--
+Victory HTML CSS Template
+https://templatemo.com/tm-507-victory
+-->
+        <title>Cerveceria de Noah - Blog</title>
+        <meta name="description" content="">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link rel="apple-touch-icon" href="apple-touch-icon.png">
+
+        <link rel="stylesheet" href="css/bootstrap.min.css">
+        <link rel="stylesheet" href="css/bootstrap-theme.min.css">
+        <link rel="stylesheet" href="css/fontAwesome.css">
+        <link rel="stylesheet" href="css/hero-slider.css">
+        <link rel="stylesheet" href="css/owl-carousel.css">
+        <link rel="stylesheet" href="css/templatemo-style.css">
+
+        <link href="https://fonts.googleapis.com/css?family=Spectral:200,200i,300,300i,400,400i,500,500i,600,600i,700,700i,800,800i" rel="stylesheet">
+        <link href="https://fonts.googleapis.com/css?family=Roboto:100,300,400,500,700,900" rel="stylesheet">
+
+        <script src="js/vendor/modernizr-2.8.3-respond-1.4.2.min.js"></script>
+    </head>
+
+<body>
+    <?php include "header.php" ?>
+    <!--/.header-->
+
+    <section class="page-heading">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-12">
+                    <h1>Nuestro blog</h1>
+                    <p>Aquí encontrarás las historias que tenemos que contarte sobre nuestro día a día.</p>
+                </div>
+            </div>
+        </div>
+    </section>
+
+
+    <section class="blog-page">
+        <div class="container">
+            <div class="row">
+                <?php
+                if($obtener_detalles->num_rows > 0) {
+                    while($fila=$obtener_detalles->fetch_assoc()) { ?>
+                        <div class="col-md-8 col-md-offset-2">
+                            <div class="blog-item">
+                                <img src="img/blog_item_01.jpg" alt="">
+                                <div class="date"><?php echo $fila['fecha'] ?></div>
+                                <div class="down-content">
+                                    <h4><?php echo $fila['titulo'] ?></h4>
+                                    <p><?php echo $fila['texto'] ?></p>
+                                </div>
+                            </div>
+                        </div>
+                <?php 
+                    }
+                } ?>
+            </div>
+        </div>
+    </section>
+
+
+    <section class="sign-up">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-12">
+                    <div class="heading">
+                        <h2>Apúntate para recibir las novedades</h2>
+                    </div>
+                </div>
+            </div>
+            <form id="contact" action="" method="get">
+                <div class="row">
+                    <div class="col-md-4 col-md-offset-3">
+                        <fieldset>
+                            <input name="email" type="text" class="form-control" id="email" placeholder="Ingresa tu email aquí..." required="">
+                        </fieldset>
+                    </div>
+                    <div class="col-md-2">
+                        <fieldset>
+                            <button type="submit" id="form-submit" class="btn">Enviar mensaje</button>
+                        </fieldset>
+                    </div>
+                </div>
+            </form>
+        </div>
+    </section>
+
+
+    <?php include "footer.php" ?>
+
+
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.2/jquery.min.js"></script>
+    <script>window.jQuery || document.write('<script src="js/vendor/jquery-1.11.2.min.js"><\/script>')</script>
+
+    <script src="js/vendor/bootstrap.min.js"></script>
+
+    <script src="js/plugins.js"></script>
+    <script src="js/main.js"></script>
+
+    <script type="text/javascript">
+    $(document).ready(function() {
+        // navigation click actions 
+        $('.scroll-link').on('click', function(event){
+            event.preventDefault();
+            var sectionID = $(this).attr("data-id");
+            scrollToID('#' + sectionID, 750);
+        });
+        // scroll to top action
+        $('.scroll-top').on('click', function(event) {
+            event.preventDefault();
+            $('html, body').animate({scrollTop:0}, 'slow');         
+        });
+        // mobile nav toggle
+        $('#nav-toggle').on('click', function (event) {
+            event.preventDefault();
+            $('#main-nav').toggleClass("open");
+        });
+    });
+    // scroll function
+    function scrollToID(id, speed){
+        var offSet = 0;
+        var targetOffset = $(id).offset().top - offSet;
+        var mainNav = $('#main-nav');
+        $('html,body').animate({scrollTop:targetOffset}, speed);
+        if (mainNav.hasClass("open")) {
+            mainNav.css("height", "1px").removeClass("in").addClass("collapse");
+            mainNav.removeClass("open");
+        }
+    }
+    if (typeof console === "undefined") {
+        console = {
+            log: function() { }
+        };
+    }
+    </script>
+</body>
+</html>
